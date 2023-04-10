8c378744f7e6ac3d489ead5a07285cc5d39ca37c
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-04-10 22:06:01 +0100

    Base code

---
+++ LightPuzzle/Light.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public class Lights
+{
+	bool lightUp = false;
+}
---
+++ LightPuzzle/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LightPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Light light1 = new Light();
+            Light light1 = new Light();
+            Light light1 = new Light();
+
+            int maxTries = 6;
+            int currentTries = 0;
+
+
+            while (currentTries < maxTries)
+            {
+                Console.WriteLine("Gimme an -a- or -s- or -d-");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "a":
+                        Changelight(1);
+                        break;
+                    case "s":
+                        Changelight(1);
+                        Changelight(2);
+                        break;
+                    case "d":
+                        Changelight(2);
+                        Changelight(3);
+                        break;
+                }
+
+                currentTries++;
+
+                Console.WriteLine($"{light1} {light2} {light3}");
+            }
+        }
+
+        public void Changelight(int lightToChange)
+        {
+            if (lightToChange == 1)
+                !light1.turnOn;
+            else if (lightToChange == 2)
+                !light2.turnOn;
+            else if (lightToChange == 3)
+                !light3.turnOn;
+        }
+    }
+}
